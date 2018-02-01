9b956a333239cc75e55f6e7413f4f02d8ff9551b
davide <the--kage91@hotmail.it>
2018-02-01 08:35:16 +0100

    Added admin interface

--- app.js
+++ app.js
@@ -6,13 +6,30 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 var db = require('./config/db');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
+// Bootstrap application models
+var modelsPath = path.join(__dirname, 'models');
+fs.readdirSync(modelsPath).forEach(function(file){
+  require(path.join(modelsPath, file));
+});
+
+// Bootstrap application api
+var apisPath = path.join(__dirname, 'api');
+fs.readdirSync(apisPath).forEach(function(file){
+  app.use('/api', require(path.join(apisPath, file)));
+});
+
+// Bootstrap webhook application
+var webhookPath = path.join(__dirname, 'webhook');
+fs.readdirSync(webhookPath).forEach(function(file){
+  app.use('/webhook', require(path.join(webhookPath, file)));
+});
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,27 +42,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
+app.use('/', index);
 app.use('/users', users);
 
-// Bootstrap application models
-var modelsPath = path.join(__dirname, 'models');
-fs.readdirSync(modelsPath).forEach(function(file){
-	require(path.join(modelsPath, file));
-});
 
-// Bootstrap application api
-var apisPath = path.join(__dirname, 'api');
-fs.readdirSync(apisPath).forEach(function(file){
-	app.use('/api', require(path.join(apisPath, file)));
-});
-
-// Bootstrap webhook application
-var webhookPath = path.join(__dirname, 'webhook');
-fs.readdirSync(webhookPath).forEach(function(file){
-	app.use('/webhook', require(path.join(webhookPath, file)));
-});
- 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var Event = require('../models/Event.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log("Qui")
+	Event
+	.aggregate([
+		{ $match: { event: 'delivered' } },
+		{ 
+			$group: {
+				_id: "$email",
+				count: { $sum: 1 },
+				
+			}
+		}, { $sort: { count: -1 }}
+	])
+	.exec(function(err, events){
+		if (err) {
+			return res.status(400).send(err);
+		};
+
+		console.log(events);
+		res.render('index', { title: 'Express', events: events });
+	})
+
 });
 
 module.exports = router;
